6437a0c0d805ce63b7a7fae4812edefb9363c3d9
jerrymao15 <jerryy.mao@gmail.com>
2021-07-01 12:14:27 -0400

    fix: remove outdated arch i386

--- ios/Widgetkit.swift
+++ ios/Widgetkit.swift
@@ -2,11 +2,11 @@ import WidgetKit
 
 @objc(Widgetkit)
 class Widgetkit: NSObject {
-    
+
     @objc(reloadAllTimelines)
     func reloadAllTimelines() -> Void {
         if #available(iOS 14.0, *) {
-            #if arch(arm64) || arch(i386) || arch(x86_64)
+            #if arch(arm64) || arch(x86_64)
                 WidgetCenter.shared.reloadAllTimelines()
             #endif
         }
@@ -15,7 +15,7 @@ class Widgetkit: NSObject {
     @objc(reloadTimelines:)
     func reloadTimelines(ofKind: String) -> Void {
         if #available(iOS 14.0, *) {
-            #if arch(arm64) || arch(i386) || arch(x86_64)
+            #if arch(arm64) || arch(x86_64)
                 WidgetCenter.shared.reloadTimelines(ofKind: ofKind)
             #endif
         }
@@ -24,16 +24,16 @@ class Widgetkit: NSObject {
     @objc(getItem:withAppGroup:withResolver:withRejecter:)
     func getItem(key: String, appGroup: String, resolve: RCTPromiseResolveBlock,reject: RCTPromiseRejectBlock) -> Void {
         var sharedDefaults: UserDefaults? = nil;
-        
+
         if(appGroup != "") {
             sharedDefaults = UserDefaults.init(suiteName: appGroup)
         }
-        
+
         if(sharedDefaults == nil) {
             resolve(nil)
             return
         }
-        
+
         let value = sharedDefaults?.value(forKey: key)
         resolve(value);
     }
@@ -41,16 +41,16 @@ class Widgetkit: NSObject {
     @objc(setItem:withValue:withAppGroup:withResolver:withRejecter:)
     func setItem(key: String, value: String, appGroup: String, resolve: RCTPromiseResolveBlock,reject: RCTPromiseRejectBlock) -> Void {
         var sharedDefaults: UserDefaults? = nil;
-        
+
         if(appGroup != "") {
             sharedDefaults = UserDefaults.init(suiteName: appGroup)
         }
-        
+
         if(sharedDefaults == nil) {
             resolve(nil)
             return
         }
-        
+
         sharedDefaults?.setValue(value, forKey: key)
         resolve(nil)
     }
