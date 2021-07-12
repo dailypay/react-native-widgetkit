33e6f4c6af16c134fed46bcaaba2fcc1c2f4e875
Jerry Mao <jerryy.mao@gmail.com>
2021-07-12 19:02:38 -0400

    Add getCurrentConfigurations + iOS 14 checks (#1)
    * fix: remove outdated arch i386
    
    * feat: add getCurrentConfigurations
    
    * feat: add ios 14 check
    
    * fix: typing

--- ios/Widgetkit.swift
+++ ios/Widgetkit.swift
@@ -2,11 +2,11 @@ import WidgetKit
 
 @objc(Widgetkit)
 class Widgetkit: NSObject {
-    
+
     @objc(reloadAllTimelines)
     func reloadAllTimelines() -> Void {
         if #available(iOS 14.0, *) {
-            #if arch(arm64) || arch(i386) || arch(x86_64)
+            #if arch(arm64) || arch(x86_64)
                 WidgetCenter.shared.reloadAllTimelines()
             #endif
         }
@@ -15,7 +15,7 @@ class Widgetkit: NSObject {
     @objc(reloadTimelines:)
     func reloadTimelines(ofKind: String) -> Void {
         if #available(iOS 14.0, *) {
-            #if arch(arm64) || arch(i386) || arch(x86_64)
+            #if arch(arm64) || arch(x86_64)
                 WidgetCenter.shared.reloadTimelines(ofKind: ofKind)
             #endif
         }
@@ -24,16 +24,16 @@ class Widgetkit: NSObject {
     @objc(getItem:withAppGroup:withResolver:withRejecter:)
     func getItem(key: String, appGroup: String, resolve: RCTPromiseResolveBlock,reject: RCTPromiseRejectBlock) -> Void {
         var sharedDefaults: UserDefaults? = nil;
-        
+
         if(appGroup != "") {
             sharedDefaults = UserDefaults.init(suiteName: appGroup)
         }
-        
+
         if(sharedDefaults == nil) {
             resolve(nil)
             return
         }
-        
+
         let value = sharedDefaults?.value(forKey: key)
         resolve(value);
     }
@@ -41,18 +41,41 @@ class Widgetkit: NSObject {
     @objc(setItem:withValue:withAppGroup:withResolver:withRejecter:)
     func setItem(key: String, value: String, appGroup: String, resolve: RCTPromiseResolveBlock,reject: RCTPromiseRejectBlock) -> Void {
         var sharedDefaults: UserDefaults? = nil;
-        
+
         if(appGroup != "") {
             sharedDefaults = UserDefaults.init(suiteName: appGroup)
         }
-        
+
         if(sharedDefaults == nil) {
             resolve(nil)
             return
         }
-        
+
         sharedDefaults?.setValue(value, forKey: key)
         resolve(nil)
+    }
+
+    // Original code: https://github.com/Taylor123/react-native-widget-center/pull/1
+    @objc(getCurrentConfigurations:reject:)
+    func getCurrentConfigurations (_ resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) -> Void {
+        if #available(iOS 14.0, *) {
+            #if arch(arm64) || arch(x86_64)
+            WidgetCenter.shared.getCurrentConfigurations {result in
+                switch result {
+                case .success(let widgetInfo):
+                    // Serialize widgets config into something the bridge understands
+                    // https://reactnative.dev/docs/native-modules-ios#argument-types
+                    var res:[[String: String]] = []
+                    for widget in widgetInfo {
+                        res.append(["kind": widget.kind, "family": widget.family.description])
+                    }
+                    resolve(res)
+                case.failure(let error):
+                    reject("404", "Couldn't get current widgets configuration", error)
+                }
+            }
+            #endif
+        }
     }
 
 
